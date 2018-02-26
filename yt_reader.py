f66b92417041aae73eb967f88e96daa50a07952f
Guillaume Milan <guillaume.milan@ensimag.grenoble-inp.fr>
2018-02-26 23:57:14 +0100

    [GRAPHIC] Command line argument -X

--- yt_reader.py
+++ yt_reader.py
@@ -1,6 +1,8 @@
+#!/usr/bin/python3
 from read_video import VideoPlayer
 from interface import CommandLineInterface
 from interface import GraphicalInterface
+import sys
 content = []
 try:
     with open(".config") as config:
@@ -9,9 +11,14 @@ except (IOError):
     print("No config file found")
     content.append('Best\n')
     content.append('Audio\n')
+interface_class = CommandLineInterface
+for i in sys.argv[1:]:
+    if i == "-X":
+        interface_class = GraphicalInterface
+
 my_player = VideoPlayer(debug=False)
 #my_interface = CommandLineInterface(my_player)
-my_interface = CommandLineInterface(my_player,debug = False)
+my_interface = interface_class(my_player,debug = False)
 my_player.start()
 my_player.set_quality(content[0][:-1])
 my_player.set_mode(content[1][:-1])
