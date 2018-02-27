ebc83ed473c7ef36ea38900d78064f638e68f03a
Guillaume Milan <guillaume.milan@ensimag.grenoble-inp.fr>
2018-02-27 23:59:51 +0100

    [GRAPHIC] Stack element dragged

--- drop_label.py
+++ drop_label.py
@@ -5,6 +5,7 @@ class DropLabel(QLabel):
     def __init__(self, title, parent):
         super().__init__(title, parent)
         self.setAcceptDrops(True)
+        self.__download_list = []
 
     def dragEnterEvent(self, e):
         if e.mimeData().hasFormat('text/plain'):
@@ -14,6 +15,12 @@ class DropLabel(QLabel):
 
     def dropEvent(self, e):
         self.setText(e.mimeData().text())
+        self.__download_list.append(e.mimeData().text())
+        print("New list element: "+self.__download_list[-1])
 
+    def getDownloadList():
+        return self.__download_list
 
+    def resetDownloadList(new_list):
+        self.__download_list = new_list
 
