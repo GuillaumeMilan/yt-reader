98b98cf44227f33fd972aa3ce828995935220cf2
Guillaume Milan <guillaume.milan@ensimag.grenoble-inp.fr>
2018-02-27 23:42:36 +0100

    [GRAPHIC] Add rescources for the drop area

---
+++ drop_label.py
@@ -0,0 +1,19 @@
+from PyQt5.QtWidgets import QLabel
+
+class DropLabel(QLabel):
+
+    def __init__(self, title, parent):
+        super().__init__(title, parent)
+        self.setAcceptDrops(True)
+
+    def dragEnterEvent(self, e):
+        if e.mimeData().hasFormat('text/plain'):
+            e.accept()
+        else:
+            e.ignore()
+
+    def dropEvent(self, e):
+        self.setText(e.mimeData().text())
+
+
+
