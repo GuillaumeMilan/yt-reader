dd561d258c3948f166172a2e72b771fdf9e37b1b
Guillaume Milan <guillaume.milan@ensimag.grenoble-inp.fr>
2018-02-28 23:00:21 +0100

    [GRAPHIC] Duration calculated in time

--- drop_label.py
+++ drop_label.py
@@ -10,7 +10,8 @@ class DropLabel(QLabel):
         super().__init__(title, parent)
         self.setAcceptDrops(True)
         self.__download_list = []
-        self.__event_function = void()
+        self.__event_function = void
+        self.__duration = 0
 
     def dragEnterEvent(self, e):
         if e.mimeData().hasFormat('text/plain'):
@@ -21,8 +22,11 @@ class DropLabel(QLabel):
     def dropEvent(self, e):
         self.setText(e.mimeData().text())
         self.__download_list.append([e.mimeData().text()])
-        print("New list element: "+self.__download_list[-1][0])
+        video = pafy.new(self.__download_list[-1][0])
+        self.__duration += video.length
         self.__event_function()
+        print("New list element: "+self.__download_list[-1][0])
+        print("New durantion :"+str(self.__duration))
 
     def connect(self, function):
         self.__event_function = function
@@ -32,10 +36,11 @@ class DropLabel(QLabel):
 
     def resetDownloadList(new_list):
         self.__download_list = new_list
+        self.__duration = self.getListDuration(new_list)
     
-    def getPlaylistDuration():
+    def getListDuration(new_list):
         duration = 0
-        for i in self.__download_list :
+        for i in new_list :
             video = pafy.new(i[0])
             duration += video.length
         return duration
