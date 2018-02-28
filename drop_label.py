5437246f8fa6424715847a01a5788e4346e52f1f
Guillaume Milan <guillaume.milan@ensimag.grenoble-inp.fr>
2018-02-28 22:14:03 +0100

    [GRAPHIC] Length of the current download list

--- drop_label.py
+++ drop_label.py
@@ -1,4 +1,8 @@
 from PyQt5.QtWidgets import QLabel
+import pafy
+
+def void():
+    pass
 
 class DropLabel(QLabel):
 
@@ -6,6 +10,7 @@ class DropLabel(QLabel):
         super().__init__(title, parent)
         self.setAcceptDrops(True)
         self.__download_list = []
+        self.__event_function = void()
 
     def dragEnterEvent(self, e):
         if e.mimeData().hasFormat('text/plain'):
@@ -15,12 +20,22 @@ class DropLabel(QLabel):
 
     def dropEvent(self, e):
         self.setText(e.mimeData().text())
-        self.__download_list.append(e.mimeData().text())
-        print("New list element: "+self.__download_list[-1])
+        self.__download_list.append([e.mimeData().text()])
+        print("New list element: "+self.__download_list[-1][0])
+        self.__event_function()
+
+    def connect(self, function):
+        self.__event_function = function
 
     def getDownloadList():
         return self.__download_list
 
     def resetDownloadList(new_list):
         self.__download_list = new_list
-
+    
+    def getPlaylistDuration():
+        duration = 0
+        for i in self.__download_list :
+            video = pafy.new(i[0])
+            duration += video.length
+        return duration
